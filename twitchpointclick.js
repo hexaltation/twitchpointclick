30e1b61f53003735e41b97f66ff7b04a2f1a21eb
Grégoire Cutzach <caztuc@hotmail.com>
2020-03-29 14:14:56 +0200

    REFACTO
    code complexity and redundancy REMOVED

--- twitchpointclick.js
+++ twitchpointclick.js
@@ -1,42 +1,43 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 const config = { attributes: true, childList: true, subtree: true };
+const modulename = 'TwitchPointClick';
 
 if (body){
-    console.log('TwitchCoinClick : body detected');
-    const callback = function(mutationsList, observer) {
-        for(let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                const targetNode = document.querySelector('.chat-input__buttons-container');
-                if (targetNode){
-                    console.log('TwitchCoinClick : chat detected')
-                    clicker(targetNode);
-                    observer.disconnect();
-                }else{
-                    console.error('TwitchCoinClick : No chat in this Twitch page');
-                }
-            }
-        }
-    };
-    const observer = new MutationObserver(callback);
-    observer.observe(body, config);
+    console.log(`${modulename} : Body detected`);
+    createObserver(body, onBodyMutation);
 } else {
-    console.error('TwitchCoinClick : no body');
+    console.error(`${modulename} : No Body`);
 }
 
-function clicker(targetNode){
-    let evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("click", true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
-    
-    const callback = function(mutationsList, observer) {
+function createObserver(target, onMutationBehaviour){
+    const callback = (mutationsList, observer) => {
         for(let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                let el = document.querySelector(".tw-button--success");
-                if (el){
-                    el.dispatchEvent(evt);
-                }
+            if (mutation.type === "childList") {
+                onMutationBehaviour(observer);
             }
         }
     };
     const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
+    observer.observe(target, config);
+}
+
+function onBodyMutation(observer){
+    const targetNode = document.querySelector(".chat-input__buttons-container");
+    if (targetNode){
+        console.log(`${modulename} : Chat detected`);
+        createObserver(targetNode, onButtonsContainerMutation);
+        observer.disconnect();
+    }else{
+        console.error(`${modulename} : No chat in this Twitch page`);
+    }
+}
+
+function onButtonsContainerMutation(observer){
+    let evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("click", true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+    let el = document.querySelector(".tw-button--success");
+    if (el){
+        el.dispatchEvent(evt);
+        console.log(`${modulename} : Points reward collected`);
+    }
 }
